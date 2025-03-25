5b4d49c2a969dd54783f80e0fe92fd20cdb43662
kardi.ib <kardi.ib@gmail.com>
2025-03-25 23:34:27 +0700

    component header logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import { cx } from '@/utils'
 import './globals.css'
 import { Inter, Manrope } from 'next/font/google'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' })
 const manrope = Manrope({ subsets: ['latin'], display: 'swap', variable: '--font-mr'  })
@@ -12,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${manrope.variable} font-mr bg-light dark:bg-dark`}>{children}</body>
+      <body 
+      className={cx(inter.variable, manrope.variable, 'font-mr bg-light dark:bg-dark')}>
+        <Header/>
+        {children}
+      </body>
     </html>
   )
 }
