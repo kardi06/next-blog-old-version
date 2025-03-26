e397a6bb5d26aa7c09fc487a53d8c18e51bc51cc
kardi.ib <kardi.ib@gmail.com>
2025-03-26 23:29:54 +0700

    header styling link and social media

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Logo from './Logo';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className='w-full p-4 px-10 flex items-center justify-between'>
         <Logo/>
+        <nav className='w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm'>
+          <Link href="/" className='mr-2'>Home</Link>
+          <Link href="/about" className='mr-2'>About</Link>
+          <Link href="/contact" className='mr-2'>Contact</Link>
+          <button>T</button>
+        </nav>
+        <div>
+          <a href='#'>Linkedin</a>
+          <a href='#'>Twitter</a>
+          <a href='#'>Github</a>
+          <a href='#'>Dribble</a>
+        </div>
     </header>
   )
 }
