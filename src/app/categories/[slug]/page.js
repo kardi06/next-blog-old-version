498a2f6b2fbb174f2447cd95f8d0eafc6ddb67e2
kardi.ib <kardi.ib@gmail.com>
2025-04-26 17:22:34 +0700

    categories page

--- src/app/categories/[slug]/page.js
+++ src/app/categories/[slug]/page.js
@@ -1,8 +1,40 @@
+import { allDocs } from "@/.contentlayer/generated";
+import BlogLayoutThree from "@/src/components/Blog/BlogLayoutThree";
+import Categories from "@/src/components/Blog/Categories";
+import { slug } from "github-slugger";
+
 const CategoryPage = ({params}) => {
+    const allCategories = ["all"];
+    const blogs = allDocs.filter((blog) => {
+        return blog.tags.some((tag) => {
+            const slugified = slug(tag);
+            if(!allCategories.includes(slugified)){
+                allCategories.push(slugified);
+            }
+            if(params.slug === 'all'){
+                return true;
+            }
+            return slugified === params.slug;
+        });
+    });
     return (
-        <div>
-            <h1>Category Name : {params.slug}</h1>
-        </div>
+        <article className="mt-12 flex flex-col text-dark dark:text-light">
+            <div className=" px-5 sm:px-10  md:px-24  sxl:px-32 flex flex-col">
+                <h1 className="mt-6 font-semibold text-2xl md:text-4xl lg:text-5xl">#{params.slug}</h1>
+                <span className="mt-2 inline-block">
+                Discover more categories and expand your knowledge!
+                </span>
+            </div>
+            <Categories categories={allCategories} currentSlug={params.slug}/>
+
+            <div className="grid  grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 grid-rows-2 gap-16 mt-5 sm:mt-10 md:mt-24 sxl:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32">
+                {blogs.map((blog, index) => (
+                <article key={index} className="col-span-1 row-span-1 relative">
+                    <BlogLayoutThree blog={blog} />
+                </article>
+                ))}
+            </div>
+        </article>
     )
 }
 
