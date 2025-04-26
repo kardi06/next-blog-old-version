84c2202e93e50b91f300af56118ea401a2c1de9b
kardi.ib <kardi.ib@gmail.com>
2025-04-26 13:43:38 +0700

    fixing toc blog page

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -29,15 +29,27 @@ export default function BlogPage({params}) {
 
             <div className="grid grid-cols-12 gap-16 mt-8 px-10">
                 <div className="col-span-4">
-                    <details>
+                    <details className='border-[1px] border-solid border-dark text-dark rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto'>
                         <summary className="font-semibold text-lg capitalize cursor-pointer">Table of Content</summary>
-                        <ul>
+                        <ul className='mt-4 font-in text-base'>
                             {
                                 blog.toc.map((heading)=> {
 
-                                    return <li key={heading.slug}>
-                                        <a href={`#${heading.slug}`}>
-                                            <span>{heading.text}</span>
+                                    return <li key={heading.slug} className='py-1'>
+                                        <a href={`#${heading.slug}`}
+                                            data-level={heading.level}
+                                            className='data-[level=two]:pl-0  data-[level=two]:pt-2
+                                            data-[level=two]:border-t border-solid border-dark/40
+                                            data-[level=three]:pl-4
+                                            sm:data-[level=three]:pl-6
+                                            flex items-center justify-start'
+                                        >
+                                             {heading.level === "three" ? (
+                                                <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
+                                                &nbsp;
+                                                </span>
+                                            ) : null}
+                                            <span className='hover:underline'>{heading.text}</span>
                                         </a>
                                     </li>
                                 })
