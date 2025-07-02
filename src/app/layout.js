e4575d28ac416810637dbf138160e12c5edc7bc0
kardi.ib <kardi.ib@gmail.com>
2025-07-02 22:03:57 +0700

    fix(main layout): fixing wrong var name siteMetadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 import { Inter, Manrope } from 'next/font/google'
 import Header from '@/src/components/Header'
 import Footer from '../components/Footer'
-import siteMetadata from '../utils/siteMetadata'
+import siteMetadata from '../utils/siteMetaData'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' })
 const manrope = Manrope({ subsets: ['latin'], display: 'swap', variable: '--font-mr'  })
